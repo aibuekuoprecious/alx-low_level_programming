f854de2f58acef4b917e344555ef51ac87f51c73
Precious Okwuoma Aibueku <aibuekuprecious@gmail.com>
2023-02-17 16:45:35 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle"
- * return 0.
+ * main - prints "Programming is like building a multilingual puzzle, followed by a new line
+ * Return: Always:0
  * /
  
 int main(void)
@@ -10,5 +10,5 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle);
 	
-	return(0);
+	return (0);
 }
