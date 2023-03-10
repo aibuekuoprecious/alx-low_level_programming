02b7fa7845ac55045fc3772eff0fbafdb3176308
Precious Okwuoma Aibueku <aibuekuprecious@gmail.com>
2023-03-10 12:37:14 -0800

    0x0A-argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,20 +8,21 @@
  * Return: the int converted from the string
  */
 
-int _atoi(char *s)		
+int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
+
 	i = 0;
 	d = 0;
 	n = 0;
 	len = 0;
 	f = 0;
 	digit = 0;
-	
+
 	while (s[len] != '\0')
 		len++;
-	
-	while (i < len && f == 0)	
+
+	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
@@ -29,7 +30,8 @@ int _atoi(char *s)
 		{
 			digit = s[i] - '0';
 			if (d % 2)
-				digit = -digit;n = n * 10 + digit;
+				digit = -digit;
+			n = n * 10 + digit;
 			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
@@ -39,18 +41,18 @@ int _atoi(char *s)
 	}
 	if (f == 0)
 		return (0);
-	return (n);	
+	return (n);
 }
 
-/**		
- * main - multiplies two numbers		
+/**
+ * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: 0 (Success), 1 (Error)		
+ * Return: 0 (Success), 1 (Error)
  */
 
-int main(int argc, char *argv[])		
+int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
@@ -63,5 +65,5 @@ int main(int argc, char *argv[])
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
 	printf("%d\n", result);
-	return (0);	
+	return (0);
 }
