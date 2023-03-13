f2fda38395d5d491a3fa11ae3a0c5cc2a5bf6471
Precious Okwuoma Aibueku <aibuekuprecious@gmail.com>
2023-03-13 06:50:12 -0700

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strdup - duplicates a string using malloc
+ * @str: the string to duplicate
+ *
+ * Return: pointer to new string, or NULL if malloc fails
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	size_t len, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	dup = malloc(sizeof(char) * (len + 1));
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		dup[i] = str[i];
+
+	dup[len] = '\0';
+
+	return (dup);
+}
+
