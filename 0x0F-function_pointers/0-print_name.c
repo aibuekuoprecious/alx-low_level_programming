83ce84b8fde23a5afac44a4bed5759a83d6c3240
Precious Okwuoma Aibueku <aibuekuprecious@gmail.com>
2023-03-22 18:19:33 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -18,25 +18,3 @@ void print_name(char *name, void (*f)(char *))
 	f(name);
 }
 
-/**
- * print_string - Prints a string
- * @str: The string to print
- */
-void print_string(char *str)
-{
-	printf("%s\n", str);
-}
-
-/**
- * main - Prints the name declared
- *
- * Return : 0
- */
-int main(void)
-{
-	/* declare name */
-	char name[] = "Aibueku Precious";
-
-	print_name(name, print_string);
-	return (0); /* return 0 */
-}
