1aa2d071449e5ccdaa6d7ccd0ad3ffb8e633c8f5
Precious Okwuoma Aibueku <aibuekuprecious@gmail.com>
2023-07-06 20:14:30 -0700

    0x1A-hash_tables

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to delete
+ *
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current *next;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (ht->array[i] != NULL)
+		{
+			next = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = next;
+		}
+	}
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht);
+}
